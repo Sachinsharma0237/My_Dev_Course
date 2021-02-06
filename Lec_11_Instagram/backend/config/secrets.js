bdb6a8c13d0032b8842570a210d9e60a917cc864
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2021-02-06 23:58:16 +0530

    mongodb introduced

---
+++ Lec_11_Instagram/backend/config/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "DB_CONFIG":"mongodb+srv://Sachin0237:12345@cluster0.btd3c.mongodb.net/insta"
+}
---
+++ Lec_11_Instagram/backend/model/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const { DB_CONFIG } = require("../config/secrets");
+
+mongoose.connect(DB_CONFIG, { useNewUrlParser: true, useUnifiedTopology: true }).then(function (obj) {
+    console.log(obj);
+  });
+
+module.exports.mongoose = mongoose;
